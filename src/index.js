f07c5f13ce160272aff044a7893ede0ef52aa383
saif ibrahim <saifkaka_66@yahoo.com>
2023-01-06 22:45:22 +0300

    creating search box

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <CardList robots={robots} />
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
