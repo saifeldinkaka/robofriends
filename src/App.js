dc96b2468d1022955db5e254c953ca6cf3e713c6
saif ibrahim <saifkaka_66@yahoo.com>
2023-01-08 00:29:10 +0300

    created search functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from './CardList'
 import { robots } from './robots';
 import SearchBox from './SearchBox'
 
 
-const App = () => {
-    return (
-        <div className='tc'>
-            < h1 > RoboFriends</h1 >
-            <SearchBox />
-            <CardList robots={robots} />
-        </div >
-    )
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+
+
+    render() {
+        const filterRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
+        return (
+            <div className='tc' >
+                <h1>RoboFriends {this.state.searchfield}</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots} />
+            </div >
+        )
+    }
 }
 
 export default App;
