67fc41a8e375896d7d040eade75826d93ba7a43d
saif ibrahim <saifkaka_66@yahoo.com>
2023-01-12 00:32:28 +0300

    scroll & cleaning code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import CardList from './CardList'
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
 import './App.css'
+import Scroll from '../components/Scroll'
 
 
 
@@ -30,21 +31,23 @@ class App extends Component {
 
 
     render() {
-        const filterRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
 
-        if (this.state.robots.length === 0) {
-            return <h1 className="tc">Loading</h1>
-        } else {
-            return (
+        return !robots.length ?
+            <h1 className="tc">Loading</h1> :
+            (
                 <div className='tc' >
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filterRobots} />
+                    <Scroll>
+                        <CardList robots={filterRobots} />
+                    </Scroll>
                 </div >
             )
-        }
+
     }
 }
 
